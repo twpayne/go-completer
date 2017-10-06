e47003cf78bb7689e135fa483e4f32176a99c8d2
Tom Payne <twpayne@gmail.com>
2017-10-06 19:46:58 +0200

    Rename arguments

--- completer.go
+++ completer.go
@@ -50,20 +50,20 @@ func (c *Completer) Add(s string) error {
 	return nil
 }
 
-// Lookup returns the unique completion of s, or the empty string and false if
-// there is no unique completion.
-func (c *Completer) Lookup(s string) (string, bool) {
-	got, ok := c.aliases[s]
+// Lookup returns the unique completion of prefix, or the empty string and
+// false if there is no unique completion.
+func (c *Completer) Lookup(prefix string) (string, bool) {
+	got, ok := c.aliases[prefix]
 	return got, ok
 }
 
-// Complete returns all possible completions of s.
-func (c *Completer) Complete(s string) []string {
+// Complete returns all possible completions of prefix.
+func (c *Completer) Complete(prefix string) []string {
 	// This is O(N*M) where N is the number of originals and M is their length.
 	// FIXME Find a more efficient implementation.
 	out := []string{}
 	for v := range c.originals {
-		if strings.HasPrefix(v, s) {
+		if strings.HasPrefix(v, prefix) {
 			out = append(out, v)
 		}
 	}
